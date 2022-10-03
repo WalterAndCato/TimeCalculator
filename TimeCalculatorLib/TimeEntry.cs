282981d3ac2b1f4b7621c8429369d8d7be869a35
Peter Steinkamp <peter.steinkamp@googlemail.com>
2022-10-03 06:46:42 +0200

    Umbau in Blazor-Projekt

---
+++ TimeCalculatorLib/TimeEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeCalculatorLib;
+
+public class TimeEntry
+{
+    public DateTime Zeit { get; set; }
+    public string Ereignisart { get; set; }
+    public string Fehlercode { get; set; }
+}
